bcd09c7c93737e0b6d699cba001e3f7d757dc6dd
zhangquan2117 <53299767+zhangquan2117@users.noreply.github.com>
2020-04-08 23:11:30 +0800

    Add files via upload

---
+++ SP/resnet.py
@@ -0,0 +1,204 @@
+
+import torch
+import torch.nn as nn
+
+class BasicBlock(nn.Module):
+    """Basic Block for resnet 18 and resnet 34
+
+    """
+
+    #BasicBlock and BottleNeck block 
+    #have different output size
+    #we use class attribute expansion
+    #to distinct
+    expansion = 1
+
+    def __init__(self, in_channels, out_channels, stride=1):
+        super().__init__()
+
+        #residual function
+        self.residual_function = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels * BasicBlock.expansion, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(out_channels * BasicBlock.expansion)
+        )
+
+        #shortcut
+        self.shortcut = nn.Sequential()
+
+        #the shortcut output dimension is not the same with residual function
+        #use 1*1 convolution to match the dimension
+        if stride != 1 or in_channels != BasicBlock.expansion * out_channels:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels * BasicBlock.expansion, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(out_channels * BasicBlock.expansion)
+            )
+        
+    def forward(self, x):
+        return nn.ReLU(inplace=True)(self.residual_function(x) + self.shortcut(x))
+
+class BottleNeck(nn.Module):
+    """Residual block for resnet over 50 layers
+
+    """
+    expansion = 4
+    def __init__(self, in_channels, out_channels, stride=1):
+        super().__init__()
+        self.residual_function = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels, stride=stride, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels * BottleNeck.expansion, kernel_size=1, bias=False),
+            nn.BatchNorm2d(out_channels * BottleNeck.expansion),
+        )
+
+        self.shortcut = nn.Sequential()
+
+        if stride != 1 or in_channels != out_channels * BottleNeck.expansion:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels * BottleNeck.expansion, stride=stride, kernel_size=1, bias=False),
+                nn.BatchNorm2d(out_channels * BottleNeck.expansion)
+            )
+        
+    def forward(self, x):
+        return nn.ReLU(inplace=True)(self.residual_function(x) + self.shortcut(x))
+
+
+class TailBottleNeck(nn.Module):
+
+    expansion = 1
+
+    def __init__(self, in_channels, out_channels, stride=1):
+        super().__init__()
+        self.residual_function = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels, stride=stride, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels * TailBottleNeck.expansion, kernel_size=1, bias=False),
+            nn.BatchNorm2d(out_channels * TailBottleNeck.expansion),
+        )
+
+        self.shortcut = nn.Sequential()
+
+        if stride != 1 or in_channels != out_channels * TailBottleNeck.expansion:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels * TailBottleNeck.expansion, stride=stride, kernel_size=1, bias=False),
+                nn.BatchNorm2d(out_channels * TailBottleNeck.expansion)
+            )
+
+    def forward(self, x):
+        return nn.ReLU(inplace=True)(self.residual_function(x) + self.shortcut(x))
+
+class ResNet34(nn.Module):
+
+    def __init__(self, block, num_block):
+        super().__init__()
+
+        self.in_channels = 64
+
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=7, padding=4, bias=False),
+            nn.BatchNorm2d(64),
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
+            nn.ReLU(inplace=True))
+        #3 4 6 3
+        self.conv2_x = self._make_layer(block, 64, num_block[0], 2)
+        self.conv3_x = self._make_layer(block, 128, num_block[1], 2)
+        self.conv4_x = self._make_layer(block, 256, num_block[2], 2)
+        self.conv5_x = self._make_layer(block, 512, num_block[3], 2)
+
+    def _make_layer(self, block, out_channels, num_blocks, stride):
+
+        strides = [stride] + [1] * (num_blocks - 1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_channels, out_channels, stride))
+            self.in_channels = out_channels * block.expansion
+        
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        output = self.conv1(x)
+        output = self.conv2_x(output)
+        output = self.conv3_x(output)
+        output = self.conv4_x(output)
+        output = self.conv5_x(output)
+        return output
+
+
+class ResNet50(nn.Module):
+
+    def __init__(self, block, num_block):
+        super().__init__()
+
+        self.in_channels = 64
+
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=7, padding=4, bias=False),
+            nn.BatchNorm2d(64),
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
+            nn.ReLU(inplace=True))
+
+        self.conv2_x = self._make_layer(block, 64, num_block[0], 1)
+        self.conv3_x = self._make_layer(block, 128, num_block[1], 2)
+        self.conv4_x = self._make_layer(block, 256, num_block[2], 2)
+        self.conv5_x = self._make_layer(block, 512, num_block[3]-1, 2)
+        self.tailBlock = TailBottleNeck(2048,512,2)
+
+    def _make_layer(self, block, out_channels, num_blocks, stride):
+        strides = [stride] + [1] * (num_blocks - 1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_channels, out_channels, stride))
+            self.in_channels = out_channels * block.expansion
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        output = self.conv1(x)
+        #print("conv1:",output.size())
+        output = self.conv2_x(output)
+        #print("conv2_x",output.size())
+        output = self.conv3_x(output)
+        #print("conv3_x",output.size())
+        output = self.conv4_x(output)
+        #print("conv4_x",output.size())
+        output = self.conv5_x(output)
+        #print("output",output.size())
+        output = self.tailBlock(output)
+
+        return output
+def resnet34():
+    """ return a ResNet 34 object
+    """
+    return ResNet34(BasicBlock, [3, 4, 6, 3])
+
+def resnet50():
+    """ return a ResNet 50 object
+    """
+    return ResNet50(BottleNeck, [3, 4, 6, 3])
+
+
+def main():
+
+    model = resnet50()  # .to(device)
+    #print(model)
+    x = torch.rand(1, 3, 128, 128)
+    y = model(x)
+    print(y.size())  # torch.Size([1, 512, 4, 4])
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
---
+++ SP/senet.py
@@ -0,0 +1,250 @@
+
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class BasicResidualSEBlock(nn.Module):
+
+    expansion = 1
+
+    def __init__(self, in_channels, out_channels, stride, r=16):
+        super().__init__()
+        self.residual = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, 3, stride=stride, padding=1),
+            #nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            
+            nn.Conv2d(out_channels, out_channels * self.expansion, 3, padding=1),
+            #nn.BatchNorm2d(out_channels * self.expansion),
+            nn.ReLU(inplace=True)
+        )
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_channels != out_channels * self.expansion:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels * self.expansion, 1, stride=stride),
+                #nn.BatchNorm2d(out_channels * self.expansion)
+            )
+        
+        self.squeeze = nn.AdaptiveAvgPool2d(1)
+        self.excitation = nn.Sequential(
+            nn.Linear(out_channels * self.expansion, out_channels * self.expansion // r),
+            nn.ReLU(inplace=True),
+            nn.Linear(out_channels * self.expansion // r, out_channels * self.expansion),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        shortcut = self.shortcut(x)
+        residual = self.residual(x)
+
+        squeeze = self.squeeze(residual)
+        squeeze = squeeze.view(squeeze.size(0), -1)
+        excitation = self.excitation(squeeze)
+        excitation = excitation.view(residual.size(0), residual.size(1), 1, 1)
+
+        x = residual * excitation.expand_as(residual) + shortcut
+
+        return F.relu(x)
+
+class BottleneckResidualSEBlock(nn.Module):
+
+    expansion = 4
+
+    def __init__(self, in_channels, out_channels, stride, r=16):
+        super().__init__()
+
+        self.residual = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, 1),
+            #nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+
+            nn.Conv2d(out_channels, out_channels, 3, stride=stride, padding=1),
+            #nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+
+            nn.Conv2d(out_channels, out_channels * self.expansion, 1),
+            #nn.BatchNorm2d(out_channels * self.expansion),
+            nn.ReLU(inplace=True)
+        )
+
+        self.squeeze = nn.AdaptiveAvgPool2d(1)
+        self.excitation = nn.Sequential(
+            nn.Linear(out_channels * self.expansion, out_channels * self.expansion // r),
+            nn.ReLU(inplace=True),
+            nn.Linear(out_channels * self.expansion // r, out_channels * self.expansion),
+            nn.Sigmoid()
+        )
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_channels != out_channels * self.expansion:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels * self.expansion, 1, stride=stride),
+                #nn.BatchNorm2d(out_channels * self.expansion)
+            )
+
+    def forward(self, x):
+
+        shortcut = self.shortcut(x)
+
+        residual = self.residual(x)
+        squeeze = self.squeeze(residual)
+        squeeze = squeeze.view(squeeze.size(0), -1)
+        excitation = self.excitation(squeeze)
+        excitation = excitation.view(residual.size(0), residual.size(1), 1, 1)
+
+        x = residual * excitation.expand_as(residual) + shortcut
+
+        return F.relu(x)
+class TailResidualSEBlock(nn.Module):
+
+    expansion = 1
+
+    def __init__(self, in_channels, out_channels, stride, r=16):
+        super().__init__()
+
+        self.residual = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, 1),
+            #nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+
+            nn.Conv2d(out_channels, out_channels, 3, stride=stride, padding=1),
+            #nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+
+            nn.Conv2d(out_channels, out_channels * self.expansion, 1),
+            #nn.BatchNorm2d(out_channels * self.expansion),
+            nn.ReLU(inplace=True)
+        )
+
+        self.squeeze = nn.AdaptiveAvgPool2d(1)
+        self.excitation = nn.Sequential(
+            nn.Linear(out_channels * self.expansion, out_channels * self.expansion // r),
+            nn.ReLU(inplace=True),
+            nn.Linear(out_channels * self.expansion // r, out_channels * self.expansion),
+            nn.Sigmoid()
+        )
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_channels != out_channels * self.expansion:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels * self.expansion, 1, stride=stride),
+                #nn.BatchNorm2d(out_channels * self.expansion)
+            )
+
+    def forward(self, x):
+
+        shortcut = self.shortcut(x)
+
+        residual = self.residual(x)
+        squeeze = self.squeeze(residual)
+        squeeze = squeeze.view(squeeze.size(0), -1)
+        excitation = self.excitation(squeeze)
+        excitation = excitation.view(residual.size(0), residual.size(1), 1, 1)
+
+        x = residual * excitation.expand_as(residual) + shortcut
+
+        return F.relu(x)
+
+class SEResNet34(nn.Module):
+
+    def __init__(self, block, block_num):
+        super().__init__()
+
+        self.in_channels = 64
+        self.pre = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=7, padding=4, bias=False),
+            nn.BatchNorm2d(64),
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
+            nn.ReLU(inplace=True))
+
+        self.stage1 = self._make_stage(block, block_num[0], 64, 2)
+        self.stage2 = self._make_stage(block, block_num[1], 128, 2)
+        self.stage3 = self._make_stage(block, block_num[2], 256, 2)
+        self.stage4 = self._make_stage(block, block_num[3], 512, 2)
+
+    
+    def forward(self, x):
+        x = self.pre(x)
+
+        x = self.stage1(x)
+        x = self.stage2(x)
+        x = self.stage3(x)
+        x = self.stage4(x)
+
+
+        return x
+
+    
+    def _make_stage(self, block, num, out_channels, stride):
+
+        layers = []
+        layers.append(block(self.in_channels, out_channels, stride))
+        self.in_channels = out_channels * block.expansion
+
+        while num - 1:
+            layers.append(block(self.in_channels, out_channels, 1))
+            num -= 1
+        
+        return nn.Sequential(*layers)
+
+
+class SEResNet50(nn.Module):
+
+    def __init__(self, block, block_num):
+        super().__init__()
+
+        self.in_channels = 64
+        self.pre = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=7, padding=4, bias=False),
+            nn.BatchNorm2d(64),
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
+            nn.ReLU(inplace=True))
+
+        self.stage1 = self._make_stage(block, block_num[0], 64, 1)
+        self.stage2 = self._make_stage(block, block_num[1], 128, 2)
+        self.stage3 = self._make_stage(block, block_num[2], 256, 2)
+        self.stage4 = self._make_stage(block, block_num[3]-1, 512, 2)
+        self.tailBlock = TailResidualSEBlock(2048, 512, 2)
+
+
+    def forward(self, x):
+        x = self.pre(x)
+
+        x = self.stage1(x)
+        x = self.stage2(x)
+        x = self.stage3(x)
+        x = self.stage4(x)
+        x = self.tailBlock(x)
+
+        return x
+
+    def _make_stage(self, block, num, out_channels, stride):
+        layers = []
+        layers.append(block(self.in_channels, out_channels, stride))
+        self.in_channels = out_channels * block.expansion
+
+        while num - 1:
+            layers.append(block(self.in_channels, out_channels, 1))
+            num -= 1
+
+        return nn.Sequential(*layers)
+
+def seresnet34():
+    return SEResNet34(BasicResidualSEBlock, [3, 4, 6, 3])
+
+def seresnet50():
+    return SEResNet50(BottleneckResidualSEBlock, [3, 4, 6, 3])
+
+def main():
+
+    model = seresnet34()  # .to(device)
+    #print(model)
+    x = torch.rand(1, 3, 128, 128)
+    y = model(x)
+    print(y.size())  # torch.Size([1, 512, 4, 4])
+
+
+if __name__ == '__main__':
+    main()
